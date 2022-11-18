ba3422c342127e916fe6ede10930a0db5889d639
preston-56 <prestonosoro56@gmail.com>
2022-11-18 12:27:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+// grab dodger
+const dodger = document.getElementById("dodger");
+
+// moves  Dodger to the Left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// listens  for events in the keyword
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+//   moves dodger to the right
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+  
+// listens for event in the keyboard
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
